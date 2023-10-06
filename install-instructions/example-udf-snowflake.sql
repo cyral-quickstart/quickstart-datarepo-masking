962bea00b3ed890f81fa9001e9eef9b0d75e1f12
Junio Cezar <jcezar@cyral.com>
2023-10-06 15:01:17 -0300

    Update Snowflake Examples

--- install-instructions/example-udf-snowflake.sql
+++ install-instructions/example-udf-snowflake.sql
@@ -1,17 +1,17 @@
 // 1. Create a new (optional) database for storing all your UDFs for custom masking
-CREATE DATABASE IF NOT EXISTS cyral;
+CREATE DATABASE IF NOT EXISTS CYRAL;
 
 // 2. Allow everyone to access the new database
-GRANT USAGE ON DATABASE cyral TO PUBLIC;
+GRANT USAGE ON DATABASE CYRAL TO PUBLIC;
 
 // 3. Create a new schema for holding the UDFs
-CREATE SCHEMA IF NOT EXISTS cyral.cyral;
+CREATE SCHEMA IF NOT EXISTS CYRAL.CYRAL;
 
 // 4. Allow everyone to access the new schema
-GRANT USAGE ON SCHEMA cyral.cyral TO PUBLIC;
+GRANT USAGE ON SCHEMA CYRAL.CYRAL TO PUBLIC;
 
 // 5. Create the new function in the target schema
-CREATE OR REPLACE FUNCTION cyral.cyral.MASK_STRING(INPUT_STRING STRING)
+CREATE OR REPLACE FUNCTION CYRAL.CYRAL."mask_string"(INPUT_STRING STRING)
   RETURNS STRING
   LANGUAGE JAVASCRIPT
 AS
@@ -29,4 +29,4 @@ $$;
 
 
 // 6. Grant the execution privilege to everyone, through the PUBLIC role
-GRANT USAGE ON FUNCTION cyral.cyral.MASK_STRING(STRING) TO PUBLIC;
+GRANT USAGE ON FUNCTION CYRAL.CYRAL."mask_string"(STRING) TO PUBLIC;
