568a8c484f8a233a04d9ea16d0365d3022088c0d
Rodolfo Reitz <rreitz@cyral.com>
2023-10-10 14:53:26 -0300

    consistent_mask for int, double and numeric

--- masking-examples/consistent-format-preserve/postgresql-udf-only.sql
+++ masking-examples/consistent-format-preserve/postgresql-udf-only.sql
@@ -1,13 +1,13 @@
-CREATE OR REPLACE FUNCTION ${SCHEMA}.consistent_mask(input_string TEXT)
+CREATE OR REPLACE FUNCTION ${SCHEMA}.consistent_mask(data TEXT)
 RETURNS TEXT AS $$
 DECLARE
   response TEXT;
   string_char CHAR(1);
   seed_base INT;
 BEGIN
-  seed_base := ABS(HASHTEXT(input_string));
-  FOR i IN 1..LENGTH(input_string) LOOP
-    string_char := SUBSTRING(input_string, i, 1);
+  seed_base := ABS(HASHTEXT(data));
+  FOR i IN 1..LENGTH(data) LOOP
+    string_char := SUBSTRING(data, i, 1);
     CASE
       WHEN string_char ~ '[A-Z]' THEN
         response := CONCAT(response, SETSEED((seed_base+i)/10000000000::DECIMAL), CHR(CAST(FLOOR(RANDOM()*(90-65+1))+65 AS INT)));
@@ -16,9 +16,54 @@ BEGIN
       WHEN string_char ~ '[0-9]' THEN
         response := CONCAT(response, SETSEED((seed_base+i)/10000000000::DECIMAL), FLOOR(RANDOM()*(9-1+1))+1);
       ELSE
-        response := CONCAT(response, SUBSTRING(input_string, i, 1));
+        response := CONCAT(response, SUBSTRING(data, i, 1));
     END CASE;
   END LOOP;
   RETURN response;
+END;
+$$ LANGUAGE plpgsql;
+
+CREATE OR REPLACE FUNCTION ${SCHEMA}.consistent_mask(data NUMERIC)
+RETURNS NUMERIC
+AS $$
+DECLARE
+  seed_base DOUBLE PRECISION;
+BEGIN
+  -- Sets a seed with [-1, 1] interval
+  -- HASHTEXT returns an int32 value: min_int32/2^31=-1 and max_int32/2^31=1
+  seed_base := HASHTEXT(data::text) / 2^31;
+  PERFORM SETSEED(seed_base);
+
+  RETURN CAST(FLOOR(RANDOM()*(data-1+1))+1 AS NUMERIC);
+END;
+$$ LANGUAGE plpgsql;
+
+CREATE OR REPLACE FUNCTION ${SCHEMA}.consistent_mask(data DOUBLE PRECISION)
+RETURNS DOUBLE PRECISION
+AS $$
+DECLARE
+  seed_base DOUBLE PRECISION;
+BEGIN
+  -- Sets a seed with [-1, 1] interval
+  -- HASHTEXT returns an int32 value: min_int32/2^31=-1 and max_int32/2^31=1
+  seed_base := HASHTEXT(data::text) / 2^31;
+  PERFORM SETSEED(seed_base);
+
+  RETURN CAST(FLOOR(RANDOM()*(data-1+1))+1 AS DOUBLE PRECISION);
+END;
+$$ LANGUAGE plpgsql;
+
+CREATE OR REPLACE FUNCTION ${SCHEMA}.consistent_mask(data INT)
+RETURNS INT
+AS $$
+DECLARE
+  seed_base DOUBLE PRECISION;
+BEGIN
+  -- Sets a seed with [-1, 1] interval
+  -- HASHTEXT returns an int32 value: min_int32/2^31=-1 and max_int32/2^31=1
+  seed_base := HASHTEXT(data::text) / 2^31;
+  PERFORM SETSEED(seed_base);
+
+  RETURN CAST(FLOOR(RANDOM()*(data-1+1))+1 AS INT);
 END;
 $$ LANGUAGE plpgsql;
