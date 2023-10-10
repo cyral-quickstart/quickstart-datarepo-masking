7cae9555eaae9b84e03040119088de0e597871c2
Rodolfo Reitz <rreitz@cyral.com>
2023-10-10 15:15:54 -0300

    Simplified cyral.consistent_mask(data TEXT)

--- masking-examples/consistent-format-preserve/postgresql-complete-script.sql
+++ masking-examples/consistent-format-preserve/postgresql-complete-script.sql
@@ -9,16 +9,21 @@ DECLARE
   string_char CHAR(1);
   seed_base INT;
 BEGIN
+  -- Sets a seed within [-1, 1] interval
+  -- HASHTEXT returns an int32 value: min_int32/2^31=-1 and max_int32/2^31=0.99999999953
+  seed_base := HASHTEXT(data) / 2^31;
+  PERFORM SETSEED(seed_base);
+
   seed_base := ABS(HASHTEXT(data));
   FOR i IN 1..LENGTH(data) LOOP
     string_char := SUBSTRING(data, i, 1);
     CASE
       WHEN string_char ~ '[A-Z]' THEN
-        response := CONCAT(response, SETSEED((seed_base+i)/10000000000::DECIMAL), CHR(CAST(FLOOR(RANDOM()*(90-65+1))+65 AS INT)));
+        response := CONCAT(response, CHR(CAST(FLOOR(RANDOM()*(90-65+1))+65 AS INT)));
       WHEN string_char ~ '[a-z]' THEN
-        response := CONCAT(response, SETSEED((seed_base+i)/10000000000::DECIMAL), CHR(CAST(FLOOR(RANDOM()*(122-97+1))+97 AS INT)));
+        response := CONCAT(response, CHR(CAST(FLOOR(RANDOM()*(122-97+1))+97 AS INT)));
       WHEN string_char ~ '[0-9]' THEN
-        response := CONCAT(response, SETSEED((seed_base+i)/10000000000::DECIMAL), FLOOR(RANDOM()*(9-1+1))+1);
+        response := CONCAT(response, FLOOR(RANDOM()*(9-1+1))+1);
       ELSE
         response := CONCAT(response, SUBSTRING(data, i, 1));
     END CASE;
