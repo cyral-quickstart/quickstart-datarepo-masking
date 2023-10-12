bfa866c9f5d01d1cc2263939cc80036b6a5366e5
Rodolfo Reitz <rreitz@cyral.com>
2023-10-12 17:46:02 -0300

    consistent_mask UDFs by type

--- masking-examples/consistent-format-preserve/sqlserver-complete-script.sql
+++ masking-examples/consistent-format-preserve/sqlserver-complete-script.sql
@@ -45,14 +45,127 @@ BEGIN
 END;
 GO
 
---CREATE OR ALTER FUNCTION cyral.consistent_mask(@unmasked_data VARCHAR(4000))
---RETURNS VARCHAR(4000) AS
---BEGIN
---  RETURN TRY_CONVERT(VARCHAR, cyral.consistent_mask(CONVERT(SQL_VARIANT, @unmasked_data)))
---END;
---GO
+CREATE OR ALTER FUNCTION cyral.consistent_mask_int(@unmasked_data INT)
+RETURNS INT AS
+BEGIN
+  RETURN TRY_CONVERT(INT, cyral.consistent_mask(CONVERT(SQL_VARIANT, @unmasked_data)))
+END;
+GO
+
+CREATE OR ALTER FUNCTION cyral.consistent_mask_varchar(@unmasked_data VARCHAR(4000))
+RETURNS VARCHAR(4000) AS
+BEGIN
+  RETURN TRY_CONVERT(VARCHAR(4000), cyral.consistent_mask(CONVERT(SQL_VARIANT, @unmasked_data)))
+END;
+GO
+
+CREATE OR ALTER FUNCTION cyral.consistent_mask_nvarchar(@unmasked_data NVARCHAR(4000))
+RETURNS NVARCHAR(4000) AS
+BEGIN
+  RETURN TRY_CONVERT(NVARCHAR(4000), cyral.consistent_mask(CONVERT(SQL_VARIANT, @unmasked_data)))
+END;
+GO
+
+CREATE OR ALTER FUNCTION cyral.consistent_mask_char(@unmasked_data CHAR)
+RETURNS CHAR AS
+BEGIN
+  RETURN TRY_CONVERT(CHAR, cyral.consistent_mask(CONVERT(SQL_VARIANT, @unmasked_data)))
+END;
+GO
+
+CREATE OR ALTER FUNCTION cyral.consistent_mask_nchar(@unmasked_data NCHAR(4000))
+RETURNS NCHAR(4000) AS
+BEGIN
+  RETURN TRY_CONVERT(NCHAR(4000), cyral.consistent_mask(CONVERT(SQL_VARIANT, @unmasked_data)))
+END;
+GO
+
+CREATE OR ALTER FUNCTION cyral.consistent_mask_bigint(@unmasked_data BIGINT)
+RETURNS BIGINT AS
+BEGIN
+  RETURN TRY_CONVERT(BIGINT, cyral.consistent_mask(CONVERT(SQL_VARIANT, @unmasked_data)))
+END;
+GO
+
+CREATE OR ALTER FUNCTION cyral.consistent_mask_numeric(@unmasked_data NUMERIC)
+RETURNS NUMERIC AS
+BEGIN
+  RETURN TRY_CONVERT(NUMERIC, cyral.consistent_mask(CONVERT(SQL_VARIANT, @unmasked_data)))
+END;
+GO
+
+CREATE OR ALTER FUNCTION cyral.consistent_mask_bit(@unmasked_data BIT)
+RETURNS BIT AS
+BEGIN
+  RETURN TRY_CONVERT(BIT, cyral.consistent_mask(CONVERT(SQL_VARIANT, @unmasked_data)))
+END;
+GO
+
+CREATE OR ALTER FUNCTION cyral.consistent_mask_smallint(@unmasked_data SMALLINT)
+RETURNS SMALLINT AS
+BEGIN
+  RETURN TRY_CONVERT(SMALLINT, cyral.consistent_mask(CONVERT(SQL_VARIANT, @unmasked_data)))
+END;
+GO
+
+CREATE OR ALTER FUNCTION cyral.consistent_mask_decimal(@unmasked_data DECIMAL)
+RETURNS DECIMAL AS
+BEGIN
+  RETURN TRY_CONVERT(DECIMAL, cyral.consistent_mask(CONVERT(SQL_VARIANT, @unmasked_data)))
+END;
+GO
+
+CREATE OR ALTER FUNCTION cyral.consistent_mask_tinyint(@unmasked_data TINYINT)
+RETURNS TINYINT AS
+BEGIN
+  RETURN TRY_CONVERT(TINYINT, cyral.consistent_mask(CONVERT(SQL_VARIANT, @unmasked_data)))
+END;
+GO
+
+CREATE OR ALTER FUNCTION cyral.consistent_mask_float(@unmasked_data FLOAT)
+RETURNS FLOAT AS
+BEGIN
+  RETURN TRY_CONVERT(FLOAT, cyral.consistent_mask(CONVERT(SQL_VARIANT, @unmasked_data)))
+END;
+GO
+
+CREATE OR ALTER FUNCTION cyral.consistent_mask_real(@unmasked_data REAL)
+RETURNS REAL AS
+BEGIN
+  RETURN TRY_CONVERT(REAL, cyral.consistent_mask(CONVERT(SQL_VARIANT, @unmasked_data)))
+END;
+GO
+
+CREATE OR ALTER FUNCTION cyral.consistent_mask_money(@unmasked_data MONEY)
+RETURNS MONEY AS
+BEGIN
+  RETURN TRY_CONVERT(MONEY, cyral.consistent_mask(CONVERT(SQL_VARIANT, @unmasked_data)))
+END;
+GO
+
+CREATE OR ALTER FUNCTION cyral.consistent_mask_smallmoney(@unmasked_data SMALLMONEY)
+RETURNS SMALLMONEY AS
+BEGIN
+  RETURN TRY_CONVERT(SMALLMONEY, cyral.consistent_mask(CONVERT(SQL_VARIANT, @unmasked_data)))
+END;
+GO
 
 -- 3. Grant the execution privilege to everyone, through the PUBLIC role
 GRANT CONNECT TO PUBLIC;
 GRANT EXECUTE ON OBJECT::cyral.consistent_mask TO PUBLIC;
---GRANT EXECUTE ON OBJECT::cyral.consistent_mask_varchar TO PUBLIC;
+GRANT EXECUTE ON OBJECT::cyral.consistent_mask_varchar TO PUBLIC;
+GRANT EXECUTE ON OBJECT::cyral.consistent_mask_int TO PUBLIC;
+GRANT EXECUTE ON OBJECT::cyral.consistent_mask_varchar TO PUBLIC;
+GRANT EXECUTE ON OBJECT::cyral.consistent_mask_nvarchar TO PUBLIC;
+GRANT EXECUTE ON OBJECT::cyral.consistent_mask_char TO PUBLIC;
+GRANT EXECUTE ON OBJECT::cyral.consistent_mask_nchar TO PUBLIC;
+GRANT EXECUTE ON OBJECT::cyral.consistent_mask_bigint TO PUBLIC;
+GRANT EXECUTE ON OBJECT::cyral.consistent_mask_numeric TO PUBLIC;
+GRANT EXECUTE ON OBJECT::cyral.consistent_mask_bit TO PUBLIC;
+GRANT EXECUTE ON OBJECT::cyral.consistent_mask_smallint TO PUBLIC;
+GRANT EXECUTE ON OBJECT::cyral.consistent_mask_decimal TO PUBLIC;
+GRANT EXECUTE ON OBJECT::cyral.consistent_mask_tinyint TO PUBLIC;
+GRANT EXECUTE ON OBJECT::cyral.consistent_mask_float TO PUBLIC;
+GRANT EXECUTE ON OBJECT::cyral.consistent_mask_real TO PUBLIC;
+GRANT EXECUTE ON OBJECT::cyral.consistent_mask_money TO PUBLIC;
+GRANT EXECUTE ON OBJECT::cyral.consistent_mask_smallmoney TO PUBLIC;
