a5d2dd91cfe083e721b2646ef3b5244f33c72352
Rodolfo Reitz <rreitz@cyral.com>
2023-10-13 18:34:19 -0300

    Oracle docs considering a dedicated package for custom UDFs

--- install-instructions/example-udf-oracle.sql
+++ install-instructions/example-udf-oracle.sql
@@ -2,22 +2,29 @@
 -- NOTE: Replace <password>
 CREATE USER CYRAL identified by "<password>";
 
--- 2. Create the new function in the target schema:
-CREATE OR REPLACE FUNCTION CYRAL."mask_string"(
-  INPUT_STRING IN VARCHAR2
-)
-RETURN VARCHAR2
-IS
-    MASKED VARCHAR2(32767) := '';
-    I NUMBER := 1;
-BEGIN
-    WHILE I <= LENGTH(INPUT_STRING) LOOP
-        MASKED := MASKED || '*';
-        I := I + 1;
-    END LOOP;
-    RETURN MASKED;
+-- 2. Create the new function in the target package:
+CREATE OR REPLACE PACKAGE CYRAL.CYRALCUSTOMPKG IS
+    FUNCTION "mask_string"(input_string IN VARCHAR2) RETURN VARCHAR2;
+END;
+/
+
+CREATE OR REPLACE PACKAGE BODY CYRAL.CYRALCUSTOMPKG AS
+    FUNCTION "mask_string"(
+      input_string IN VARCHAR2
+    )
+    RETURN VARCHAR2
+    IS
+        masked VARCHAR2(32767) := '';
+        i NUMBER := 1;
+    BEGIN
+        WHILE i <= LENGTH(input_string) LOOP
+            masked := masked || '*';
+            i := i + 1;
+        END LOOP;
+        RETURN masked;
+    END;
 END;
 /
 
 -- 3. Grant the execution privilege to everyone, through the PUBLIC role
-GRANT EXECUTE ON CYRAL."mask_string" TO PUBLIC;
+GRANT EXECUTE ON CYRAL.CYRALCUSTOMPKG TO PUBLIC;
--- masking-examples/consistent-format-preserve/oracle-udf-only.sql
+++ masking-examples/consistent-format-preserve/oracle-udf-only.sql
@@ -1,36 +1,36 @@
-CREATE OR REPLACE FUNCTION ${USER_SCHEMA}."consistent_mask_varchar"(
-    data IN VARCHAR2
-)
-RETURN VARCHAR2
-IS
-    masked_data VARCHAR2(32767) := '';
-    hash RAW(32);
-BEGIN
-    hash := DBMS_CRYPTO.HASH(UTL_I18N.STRING_TO_RAW(data, 'AL32UTF8'), DBMS_CRYPTO.HASH_SH256);
-    DBMS_RANDOM.SEED(hash);
-    FOR i IN 1..LENGTH(data) LOOP
-       CASE
-           WHEN regexp_like(SUBSTR(data, i, 1), '[A-Z]') THEN
-               masked_data := CONCAT(masked_data, CHR(FLOOR(DBMS_RANDOM.VALUE()*(90-65+1))+65));
-           WHEN regexp_like(SUBSTR(data, i, 1), '[a-z]') THEN
-               masked_data := CONCAT(masked_data, CHR(FLOOR(DBMS_RANDOM.VALUE()*(122-97+1))+97));
-           WHEN regexp_like(SUBSTR(data, i, 1), '[0-9]') THEN
-               masked_data := CONCAT(masked_data, FLOOR(DBMS_RANDOM.VALUE()*(9-1+1))+1);
-           ELSE
-               masked_data := CONCAT(masked_data, SUBSTR(data, i, 1));
-       END CASE;
-    END LOOP;
-
-    RETURN masked_data;
+CREATE OR REPLACE PACKAGE ${USER_SCHEMA}.${PACKAGE} IS
+    FUNCTION "consistent_mask"(data IN VARCHAR2) RETURN VARCHAR2;
+    FUNCTION "consistent_mask"(data IN NUMBER) RETURN NUMBER;
 END;
 /
 
-CREATE OR REPLACE FUNCTION ${USER_SCHEMA}."consistent_mask_number"(
-    data IN NUMBER
-)
-RETURN NUMBER
-IS
-BEGIN
-    RETURN ${USER_SCHEMA}."consistent_mask_varchar"(data);
+CREATE OR REPLACE PACKAGE BODY ${USER_SCHEMA}.${PACKAGE} AS
+    FUNCTION "consistent_mask"(data IN VARCHAR2) RETURN VARCHAR2
+	IS
+	    masked_data VARCHAR2(32767) := '';
+	    hash RAW(32);
+	BEGIN
+	    hash := DBMS_CRYPTO.HASH(UTL_I18N.STRING_TO_RAW(data, 'AL32UTF8'), DBMS_CRYPTO.HASH_SH256);
+	    DBMS_RANDOM.SEED(hash);
+	    FOR i IN 1..LENGTH(data) LOOP
+	       CASE
+	           WHEN regexp_like(SUBSTR(data, i, 1), '[A-Z]') THEN
+	               masked_data := CONCAT(masked_data, CHR(FLOOR(DBMS_RANDOM.VALUE()*(90-65+1))+65));
+	           WHEN regexp_like(SUBSTR(data, i, 1), '[a-z]') THEN
+	               masked_data := CONCAT(masked_data, CHR(FLOOR(DBMS_RANDOM.VALUE()*(122-97+1))+97));
+	           WHEN regexp_like(SUBSTR(data, i, 1), '[0-9]') THEN
+	               masked_data := CONCAT(masked_data, FLOOR(DBMS_RANDOM.VALUE()*(9-1+1))+1);
+	           ELSE
+	               masked_data := CONCAT(masked_data, SUBSTR(data, i, 1));
+	       END CASE;
+	    END LOOP;
+
+	    RETURN masked_data;
+	END;
+    FUNCTION "consistent_mask"(data IN NUMBER) RETURN NUMBER
+    IS
+    BEGIN
+        RETURN "consistent_mask"(TO_CHAR(data));
+    END;
 END;
 /
