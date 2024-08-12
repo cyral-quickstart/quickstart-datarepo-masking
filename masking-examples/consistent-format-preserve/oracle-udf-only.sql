7f6ac47c058fa66cbe6e886ab0af6ac6f8b69543
Chris Campo <ccampo133@users.noreply.github.com>
2024-08-12 12:50:23 -0400

    Add Oracle UDF

--- masking-examples/consistent-format-preserve/oracle-udf-only.sql
+++ masking-examples/consistent-format-preserve/oracle-udf-only.sql
@@ -1,6 +1,8 @@
 CREATE OR REPLACE PACKAGE ${USER_SCHEMA}.${PACKAGE} IS
     FUNCTION "consistent_mask"(data IN VARCHAR2) RETURN VARCHAR2;
     FUNCTION "consistent_mask"(data IN NUMBER) RETURN NUMBER;
+    FUNCTION "consistent_mask_hash"(data IN VARCHAR2) RETURN VARCHAR2;
+    FUNCTION "consistent_mask_hash"(data IN NUMBER) RETURN NUMBER;
 END;
 /
 
@@ -27,10 +29,51 @@ CREATE OR REPLACE PACKAGE BODY ${USER_SCHEMA}.${PACKAGE} AS
 
 	    RETURN masked_data;
 	END;
+
     FUNCTION "consistent_mask"(data IN NUMBER) RETURN NUMBER
     IS
     BEGIN
         RETURN "consistent_mask"(TO_CHAR(data));
+    END;
+
+    FUNCTION "consistent_mask_hash"(data IN VARCHAR2) RETURN VARCHAR2
+    IS
+        resp       VARCHAR2(32767) := '';
+        hash_hex   VARCHAR2(64);
+        c          CHAR(1);
+    BEGIN
+        -- Oracle is mostly 1-based, but for consistency with other languages, we
+        -- use 0-based indexing here. This requires adding 1 in a few spots below.
+        FOR i IN 0..(LENGTH(data) - 1)
+            LOOP
+                c := SUBSTR(data, i + 1, 1);
+                hash_hex := RAWTOHEX(DBMS_CRYPTO.HASH(UTL_I18N.STRING_TO_RAW(TO_CHAR(i) || data, 'AL32UTF8'), DBMS_CRYPTO.HASH_SH256));
+                -- Explanation of some magic numbers below:
+                -- 26 is the number of letters in the ISO Latin alphabet.
+                -- 65 is the ASCII code for 'A'.
+                -- 97 is the ASCII code for 'a'.
+                -- 48 is the ASCII code for '0'.
+                -- 10 is the number of digits (0-9).
+                CASE
+                    WHEN c BETWEEN 'A' AND 'Z' THEN
+                        -- Oracle's strings are 1-based, so we need to add 1 when
+                        -- passing the MOD'ed index to SUBSTR.
+                        resp := resp || CHR(MOD(ASCII(c) - 65 + TO_NUMBER(SUBSTR(hash_hex, MOD(i, LENGTH(hash_hex)) + 1, 1), 'X'), 26) + 65);
+                    WHEN c BETWEEN 'a' AND 'z' THEN
+                        resp := resp || CHR(MOD(ASCII(c) - 97 + TO_NUMBER(SUBSTR(hash_hex, MOD(i, LENGTH(hash_hex)) + 1, 1), 'X'), 26) + 97);
+                    WHEN c BETWEEN '0' AND '9' THEN
+                        resp := resp || CHR(MOD(ASCII(c) - 48 + TO_NUMBER(SUBSTR(hash_hex, MOD(i, LENGTH(hash_hex)) + 1, 1), 'X'), 10) + 48);
+                    ELSE
+                        resp := resp || c;
+                    END CASE;
+            END LOOP;
+        RETURN resp;
+    END;
+
+    FUNCTION "consistent_mask_hash"(data IN NUMBER) RETURN NUMBER
+    IS
+    BEGIN
+        RETURN "consistent_mask_hash"(TO_CHAR(data));
     END;
 END;
 /
